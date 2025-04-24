ac798832d59ea4ba334fa67c150f7a2d526e9923
Pablo Garzon <pablogarzon01@gmail.com>
2025-04-23 22:49:23 -0500

    Create new function named -generar_numero_aleatorio -

--- Proyecto_1/Script1.sql
+++ Proyecto_1/Script1.sql
@@ -1,7 +1,38 @@
 -- Insertar datos de una base de datos a otra con tablas y cambos que coinciden
 
+-- IMPORTANTE HABILITAR EL USO DE FUNCIONES E MYSQL
+SET GLOBAL log_bin_trust_function_creators = 1;
+
 INSERT INTO empresa.tb_items_vendidos
 SELECT NUMERO, CODIGO_DEL_PRODUCTO AS CODIGO, CANTIDAD, PRECIO FROM jugos_ventas.items_facturas;
 
 INSERT INTO empresa.tb_venta
 SELECT NUMERO, FECHA_VENTA AS FECHA, DNI, MATRICULA, IMPUESTO FROM jugos_ventas.facturas;
+
+-- Consulta de tablas agrupadas 
+
+SELECT * FROM tb_venta V
+INNER JOIN  tb_items_vendidos I
+ON V.NUMERO = I.NUMERO; 
+
+/* Teniendo en cuenta la base de datos que tenenmos, esta cuenta con diferentes entidades
+tales como cliente, vendedor, producto, factura e items, por lo que se puede realizar
+operacione sde transacciones entre estas tablas.
+
+Realizaremos un store procedure para generar una factura, con el DNI del cliente y matricula del vendedor
+considerando una parametros de entrada tales como FECHA, NUM. MAX ITEMS, NUM. MAX CANTIDAD  para los items
+y adicionalmente realizaremos una funcion que genere un numero aleatorio para los productos de essa factura
+contenindo CODIGO, PRECIO, CANTIDAD
+ */
+
+ SELECT FLOOR((RAND()*(250 - 20 + 1) + 20)) AS ALEATORIO;  -- Numeros aleatorios entre 0 y 1
+
+ DELIMITER $$
+CREATE FUNCTION generar_numero_aleatorio(min INT, max INT)
+RETURNS INT
+BEGIN
+    DECLARE vresultado INT;
+    SET vresultado = FLOOR((RAND() * (max - min + 1)) + min);
+    RETURN vresultado;
+END$$
+DELIMITER ;
